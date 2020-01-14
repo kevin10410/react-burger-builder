5bba2915d3e1decd432e2f79e94ef411a3f8da6e
OTree <kevin10410@gmail.com>
2020-01-14 22:07:34 +0800

    refactor(App): implement Wrapper component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Wrapper from './components/Wrapper/Wrapper';
 
 class App extends Component {
   render() {
     return(
-      <div></div>
+      <Wrapper>
+        <p>Test Content</p>
+      </Wrapper>
     );
   }
 }
