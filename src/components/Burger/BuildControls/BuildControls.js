4c9632b82ed52d23cf72f0fb9f2e0b4c06f99bf9
kevin10410 <kevin10410@gmail.com>
2020-01-29 21:16:14 +0800

    refactor(BuildControls): create BuildControls component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import BuildControl from './BuildControl';
+
+const DivBuildControls = styled.div`
+  width: 100%;
+  background-color: #CF8F2E;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 2px 1px #ccc;
+  margin: auto;
+  padding: 10px 0;
+`;
+
+const controls = [
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Salad', type: 'salad' },
+];
+
+const BuildControls = (props) => (
+  <DivBuildControls>
+    {
+      controls.map(ctrl =>
+        <BuildControl
+          key = {ctrl.label}
+          label = {ctrl.label}
+        />
+      )
+    }
+  </DivBuildControls>
+);
+
+export default BuildControls;
