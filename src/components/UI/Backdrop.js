c6fba4dbea43d566c6a66903786446423b0a7f6f
kevin10410 <kevin10410@gmail.com>
2020-01-31 23:06:24 +0800

    refactor(Backdrop): create Backdrop component

---
+++ src/components/UI/Backdrop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DivBackdrop = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Backdrop = (props) => (
+  props.show &&
+  <DivBackdrop
+    onClick = { () => { props.cancelPurchase() } }
+  />
+);
+
+export default Backdrop;
