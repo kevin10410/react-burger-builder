28880f78a6ec7eae6d3dc650dd53ee1eee902aac
OTree <kevin10410@gmail.com>
2020-02-02 21:53:15 +0800

    refactor(NavigationList): create NavigationList component

---
+++ src/components/Navigation/NavigationList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import NavigationItem from './NavigationItem';
+
+const UlNavigationList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  height: 100%;
+
+  @media (min-width: 500px) {
+    & {
+      flex-flow: row;
+    }
+  }
+`;
+
+const NavigationList = (props) => (
+  <UlNavigationList>
+    <NavigationItem
+      link = "/"
+    >Burger Builder</NavigationItem>
+    <NavigationItem
+      link = "/"
+    >Checkout</NavigationItem>
+  </UlNavigationList>
+);
+
+export default NavigationList;
