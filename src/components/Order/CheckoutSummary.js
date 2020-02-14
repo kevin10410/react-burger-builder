d33eba8b761b89b32ed05f437735c8f8ce3bdb89
OTree <kevin10410@gmail.com>
2020-02-14 22:39:16 +0800

    refactor(CheckoutSummary): create CheckoutSummary component

---
+++ src/components/Order/CheckoutSummary.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import Burger from '../Burger/Burger';
+import { ButtonDanger, ButtonSuccess } from '../../component-style/ButtonStyle';
+
+const DivCheckoutSummary = styled.div`
+  text-align: center;
+  width: 80%;
+  margin: auto;
+
+  @media (min-width: 600px) {
+    width: 700px;
+  }
+`;
+
+const DivBurgerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: auto;
+`;
+
+const CheckoutSummary = (props) => (
+  <DivCheckoutSummary>
+    <h1>We Hope it tastes well!</h1>
+    <DivBurgerWrapper>
+      <Burger
+        ingredients = { props.ingredients }
+      />
+    </DivBurgerWrapper>
+    <ButtonDanger
+      onClick = { () => {} }
+    >CANCEL</ButtonDanger>
+    <ButtonSuccess
+      onClick = { () => {} }
+    >CONTINUE</ButtonSuccess>
+  </DivCheckoutSummary>
+);
+
+export default CheckoutSummary;
