2a158b3b83ab1fc783d007582f2247b0e3beb148
OTree <kevin10410@gmail.com>
2020-02-15 23:15:41 +0800

    refactor(ContactData): create ContactData component

---
+++ src/components/Order/ContactData.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import { ButtonSuccess } from '../../component-style/ButtonStyle';
+
+const DivContactData = styled.div`
+  margin: 20px auto;
+  width: 80%;
+  text-align: center;
+  box-shadow: 0 2px 3px #CCC;
+  border: 1px solid #eee;
+  padding: 10px;
+  box-sizing: border-box;
+
+  @media (min-width: 600px) {
+    width: 500px;
+  }
+`;
+
+const Input = styled.input`
+  display: block;
+`;
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: '',
+    },
+  }
+  
+  render() {
+    return (
+      <DivContactData>
+        <h4>Enter your Contact Data</h4>
+        <form>
+          <Input type="text"
+            name="name"
+            placeholder="Your name"
+          />
+          <Input type="text"
+            name="email"
+            placeholder="Your email"
+          />
+          <Input type="text"
+            name="street"
+            placeholder="Street"
+          />
+          <Input type="text"
+            name="postal"
+            placeholder="Postal Code"
+          />
+          <ButtonSuccess
+          >Order</ButtonSuccess>
+        </form>
+      </DivContactData>
+    );
+  };
+}; 
+
+export default ContactData;
