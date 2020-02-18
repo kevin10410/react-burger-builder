ea48a8dbd0d436b2ba5722fd6226bd146729e4b1
OTree <kevin10410@gmail.com>
2020-02-18 12:01:31 +0800

    refactor(Orders): implement fetch & render mock data & render

--- src/components/Orders/index.js
+++ src/components/Orders/index.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
 import Order from '../Order/Order';
+import Spinner from '../UI/Spinner';
+
+import { getOrders } from '../../api/orderService';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: false,
+  };
+
+  startLoading = () => {
+    this.setState({ loading: true });
+  }
+
+  endLoading = () => {
+    this.setState({ loading: false });
+  }
+
+  async componentDidMount() {
+    this.startLoading();
+    await getOrders()
+      .then(res => res.data)
+      .then(orders => {
+        this.setState({ orders });
+      })
+      .catch(err => console.log(err));
+    this.endLoading();
+  };
+
   render() {
+    const Orders = this.state.orders
+      .map(order => (<Order {...order}/>));
+
     return (
       <div>
-        <Order/>
-        <Order/>
+        { 
+          this.state.loading
+            ? <Spinner/>
+            : Orders 
+        }
       </div>
     );
   }
