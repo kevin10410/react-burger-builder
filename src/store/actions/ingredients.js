c95a4dae94cf1a9285b78083fc84f17e196a87c1
OTree <kevin10410@gmail.com>
2020-02-21 17:11:33 +0800

    refactor(actions): add ingredients actionCreators

---
+++ src/store/actions/ingredients.js
@@ -0,0 +1,9 @@
+export const ADD_INGREDIENT = (ingredient) => ({
+  type: 'ADD_INGREDIENT',
+  payload: ingredient,
+});
+
+export const SUBSTRACT_INGREDIENT = (ingredient) => ({
+  type: 'SUBSTRACT_INGREDIENT',
+  payload: ingredient,
+});
