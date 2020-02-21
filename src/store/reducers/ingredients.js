d85db95e079a4ac8d9b9dac1fbcc7601dc626d74
OTree <kevin10410@gmail.com>
2020-02-21 17:12:30 +0800

    refactor(reducers): add reducerIngredients

---
+++ src/store/reducers/ingredients.js
@@ -0,0 +1,37 @@
+import {
+  ADD_INGREDIENT,
+  SUBSTRACT_INGREDIENT,
+} from '../actionTypes';
+
+const initState = {
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
+};
+
+const reducerIngredients = (state = initState, action) => {
+  const { type, payload } = action;
+  const newState = {
+    ingredients: {
+      ...state.ingredients,
+    },
+};
+
+  switch (type) {
+    case ADD_INGREDIENT:
+      newState.ingredients[payload] += 1;
+      return newState;
+    case SUBSTRACT_INGREDIENT:
+      newState.ingredients[payload] = newState.ingredients[payload] > 0
+        ? newState.ingredients[payload] -= 1
+        : 0;
+      return newState;
+    default:
+      return newState;
+  };
+};
+
+export default reducerIngredients;
