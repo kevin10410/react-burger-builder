e1440b9cca6b798288d8ab87b08681d6c66ac27b
OTree <kevin10410@gmail.com>
2020-02-26 11:11:25 +0800

    refactor(cookieUtils): add deleteCookie method

--- src/utils/cookieUtils.js
+++ src/utils/cookieUtils.js
@@ -10,3 +10,7 @@ export const getCookie = (key) => {
       ? matchCookie[2]
       : null;
 };
+
+export const deleteCookie = (key) => {
+  document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+};
